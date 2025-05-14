6e3435ac2c3fb1f5cfe2ad1e789ca245886d9b8e
Fernanda Ruiz <117100019+FerRuizDevp@users.noreply.github.com>
2025-05-14 13:23:35 -0400

    Initial commit

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useMovieContext } from "../context/MovieContext";
+import "../css/MovieCard.css";
+
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+function MovieCard({ movie }) {
+  const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+  const favorite = isFavorite(movie.id);
+
+  function onFavoriteClick(e) {
+    e.preventDefault();
+    if (favorite) removeFromFavorites(movie.id);
+    else addToFavorites(movie);
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img
+          src={
+            movie.poster_path
+              ? `${IMAGE_BASE_URL}${movie.poster_path}`
+              : "/placeholder.png"
+          }
+          alt={movie.title}
+        />
+        <div className="movie-overlay">
+          <button
+            className={`favorite-btn ${favorite ? "active" : ""}`}
+            onClick={onFavoriteClick}
+          >
+            <span>♥</span>
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date?.split("-")[0]}</p>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
